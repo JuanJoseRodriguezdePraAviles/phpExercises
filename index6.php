fb2c835002da746e74dbb82f446bae8ea645e6c0
Juan José Rodríguez de Pra Avilés <jjpra196@gmail.com>
2025-05-19 16:14:24 +0200

    exercise 6

---
+++ index6.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Acceder la página con un query param (localhost:8000/index4.php?id=1). Conectar a la base de datos de MySQL utilizando mysqli. Hacer una consulta para obtener una habitación (con el ID de GET) y mostrarla abajo utilizando el código de index4.php
+*/
+$id = $_GET['id'];
+
+$mysqli = new mysqli("127.0.0.1","devuser","1234","hotel");
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+$sql = "SELECT * FROM room WHERE id={$id}";
+$result = $mysqli -> query($sql);
+
+if($result) {
+    $room = $result->fetch_assoc();
+
+    if($room['id']===$id){
+        echo "<pre>";
+        echo "Name:".$room['room_name']."<br/>"."Number:".$room['number']."<br/>"."Price:".$room['price']."<br/>"."Discount:".$room['discount']."<br/>";
+        echo "</pre>";
+    }
+}
+    
