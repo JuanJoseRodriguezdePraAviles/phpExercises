9fc3f8373fb1a771e073746099e450d0295bf18b
Juan José Rodríguez de Pra Avilés <jjpra196@gmail.com>
2025-05-21 09:58:29 +0200

    exercise Multiply codeWars

---
+++ codeWars/multiply.php
@@ -0,0 +1,10 @@
+<?php
+//This code does not execute properly. Try to figure out why.
+function multiply(int $a, int $b): int {
+    return $a * $b;
+}
+
+const num1 = 5;
+const num2 = 2;
+print(num1." * ".num2." = ".multiply(num1, num2));
+
